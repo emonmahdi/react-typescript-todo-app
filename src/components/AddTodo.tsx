a725bafb5cc3f2efecce52b6097aa7c46fb885c8
emonmahdi <developeremon1997@gmail.com>
2024-01-24 22:28:32 +0600

    create form element with types

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
+
 const AddTodo = () => {
+  const [input, setInput] = useState<string>('')
   return (
     <div>
-      <h2>Add to do</h2>
+      <form>
+        <div className="flex items-center w-full max-w-lg gap-2 p-5 m-auto">
+          <input
+            type="text"
+            value={input}
+            onChange={e => setInput(e.target.value)}
+            placeholder="add todo"
+            className="w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-600 rounded-xl placeholder:text-zinc-500 focus:border-white"
+          />
+          <button
+            type="submit"
+            className="px-5 py-2 text-sm font-normal text-blue-300 bg-blue-900 border-2 border-blue-900 active:scale-95 rounded-xl"
+          >
+            Add Todo
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
